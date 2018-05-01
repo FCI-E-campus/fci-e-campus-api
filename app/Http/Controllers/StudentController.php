55ab6eecf753d1b542dd4b9e03ca118a6d859f0c
AbooodMahmoud <abod.mahmoud92@gmail.com>
2018-05-01 02:09:45 +0200

    Update StudentController.php

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -66,8 +66,7 @@ class StudentController extends Controller
      */
     public function edit()
     {
-        $student = new Student();
-        return $student->activate("samehh","9263200");
+      
     }
 
     /**
