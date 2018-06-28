ea24c54bbfc9fdefb9111da7a7d9362494565b17
ABD-ULZIZMAHMOUD <zizomody2511@gmail.com>
2018-06-28 23:35:52 +0200

    edit14

--- app/Course.php
+++ app/Course.php
@@ -213,14 +213,13 @@ class Course extends Model
 
         //zizomody
         $courses=Course::where('COURSECODE',$courseCode)->get();
-        return $courses;
         $c=$courses[0];
         $profe=ProfessorCource::select('PROFUSERNAME')->where('COURSECODE', $courses[0]["COURSECODE"])->get();
         $ta=TACourse::select('TAUSERNAME')->where('COURSECODE', $courses[0]["COURSECODE"])->get();
         $res=array("Course"=>$c,"prof"=>$profe,"ta"=>$ta);
         $subJason =array("status"=>"success","result"=>$res);
-      //  return  $subJason;
-        return null;
+       return  $subJason;
+
     }
     //retrieve courses on the system
     public function showCoursesOnTheSystem()
