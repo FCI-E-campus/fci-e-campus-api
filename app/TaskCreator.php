3bf362632cb91d5d8158b47d4b93b7b1e1f47182
ABD-ULZIZMAHMOUD <zizomody2511@gmail.com>
2018-07-02 04:52:40 +0200

    edit 41

--- app/TaskCreator.php
+++ app/TaskCreator.php
@@ -14,6 +14,11 @@ class TaskCreator extends Model
     {
         $upload = new TaskCreator();
         //CREATORID	CREATORUSERNAME	CREATORTYPE
+        $num= TaskCreator::where('CREATORUSERNAME', $un)->count();
+        if ($num!=0){
+            $x= TaskCreator::where('CREATORUSERNAME', $un)->get();
+            return $x[0]["CREATORID"];
+        }
         $upload-> CREATORUSERNAME= $un;
         $upload-> CREATORTYPE= $type;
         $upload->save();
